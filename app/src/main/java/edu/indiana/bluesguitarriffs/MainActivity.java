c7321cfe84b9c9539d37ac96aa85a72b33da7b68
Alex Totheroh <atothero@indiana.edu>
2016-10-29 21:08:57 -0400

    skeleton in place

--- app/src/main/java/edu/indiana/bluesguitarriffs/MainActivity.java
+++ app/src/main/java/edu/indiana/bluesguitarriffs/MainActivity.java
@@ -13,14 +13,16 @@
 
 package edu.indiana.bluesguitarriffs;
 
-import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -31,24 +33,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ActionBar actionBar = getActionBar();
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         final ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
-        pager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager()));
+        //setSupportActionBar(toolbar);
 
-        pager.setOnTouchListener(new View.OnTouchListener() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Page 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Page 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Page 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Page 4"));
+        tabLayout.addTab(tabLayout.newTab().setText("Page 5"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        pager.setAdapter(new MyFragmentPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount()));
+        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
 
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                pager.getParent().requestDisallowInterceptTouchEvent(true);
-                return false;
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
     }
 
-    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-        public MyFragmentPagerAdapter(FragmentManager fm) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+        int mNumberOfTabs;
+
+        public MyFragmentPagerAdapter(FragmentManager fm, int tabs) {
             super(fm);
+            this.mNumberOfTabs = tabs;
         }
 
         @Override
@@ -65,12 +104,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 5;
+            return this.mNumberOfTabs;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return "Page " + (position + 1);
+
+            //return "Page " + (position + 1);
+            return "";
         }
     }
 }
--- app/src/main/java/edu/indiana/bluesguitarriffs/PageFragment.java
+++ app/src/main/java/edu/indiana/bluesguitarriffs/PageFragment.java
@@ -14,8 +14,8 @@ public class PageFragment extends Fragment {
         int pageNumber = getArguments().getInt("page_number");
         View v = inflater.inflate(R.layout.page_layout, container, false);
 
-        TextView tv = (TextView)v.findViewById(R.id.PageTitle);
-        tv.setText("Page " + Integer.toString(pageNumber));
+        //TextView tv = (TextView)v.findViewById(R.id.PageTitle);
+        //tv.setText("Page " + Integer.toString(pageNumber));
 
         return v;
     }
