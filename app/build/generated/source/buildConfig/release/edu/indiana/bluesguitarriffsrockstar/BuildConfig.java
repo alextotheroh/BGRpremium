7b91db4d04b7be13a6906fb403d8cdf5ba245d4c
Alex Totheroh <atothero@indiana.edu>
2016-11-05 02:13:45 -0400

    updated version for beta build 1.1

---
+++ app/build/generated/source/buildConfig/release/edu/indiana/bluesguitarriffsrockstar/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.indiana.bluesguitarriffsrockstar;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "edu.indiana.bluesguitarriffsrockstar";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1";
+}
