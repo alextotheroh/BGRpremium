ea5d753bf7a58d2fbfe415712fe08f77bb4bc700
Alex Totheroh <atothero@indiana.edu>
2016-11-12 21:25:33 -0500

    remastered riffs, turned down volume a little also

--- app/src/main/java/edu/indiana/bluesguitarriffsrockstar/RiffDetailActivity.java
+++ app/src/main/java/edu/indiana/bluesguitarriffsrockstar/RiffDetailActivity.java
@@ -1,6 +1,9 @@
 package edu.indiana.bluesguitarriffsrockstar;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
@@ -8,16 +11,19 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class RiffDetailActivity extends Activity {
 
     String riffNumber = "1";
+    int riffNumberAsInt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riff_detail);
         Bundle bundle = getIntent().getExtras();
+        this.riffNumberAsInt = bundle.getInt("riffNumber");
         this.riffNumber = Integer.toString(bundle.getInt("riffNumber"));
         setContentView(R.layout.activity_riff_detail);
 
@@ -36,9 +42,34 @@ public class RiffDetailActivity extends Activity {
             tabImage.setLayoutParams(params);
         }
 
+        // show rateBeg if on riff 50 and set onclick listener
+        if (riffNumber.equals("50")) {
+            ImageButton rateBegButton = (ImageButton)findViewById(R.id.rateBeg);
+
+            rateBegButton.setVisibility(View.VISIBLE);
+
+            rateBegButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                    Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                    try {
+                        startActivity(myAppLinkToMarket);
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(getApplicationContext(), "Unable to find market app", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
         // set audio to play
         int audioFileIdentifier = getApplicationContext().getResources().getIdentifier("riff" + riffNumber, "raw", getApplicationContext().getPackageName());
         final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), audioFileIdentifier);
+
+        // slightly turn down volume if not on riff 1-10
+        if (riffNumberAsInt > 10) {
+            mp.setVolume(0.8f, 0.8f);
+        }
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
